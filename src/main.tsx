e6b61ac0865a54b7167efce8797ef024eeeb0e7b
Natali Zikrach <mne310872@yandex.ru>
2024-02-14 14:26:44 +0300

    feat: add redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
-import { store } from './redux/app/store.ts';
+import { store, persistor } from './redux/app/store.ts';
 import './index.scss';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/redux/app/store.ts
+++ src/redux/app/store.ts
@@ -1,19 +1,43 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from '../store/userSlice';
 import favouriteUserReducer from '../store/favouriteUserSlice';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistStore,
+} from 'redux-persist';
+
+const persistconfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['favouriteUsers'],
+};
 
 const rootReducer = combineReducers({
   users: userReducer,
   favouriteUsers: favouriteUserReducer,
 });
 
+const persistedreducer = persistReducer(persistconfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedreducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
